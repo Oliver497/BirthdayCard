46c19f1ba24ca6672da5f85560402c80378a9dad
Oliver497 <117872009+Oliver497@users.noreply.github.com>
2023-04-19 15:29:09 -0500

    BirthdayCard files creation

---
+++ app/src/main/java/com/example/birthdaycard/BasicImageDownloader.java
@@ -0,0 +1,4 @@
+package com.example.birthdaycard;
+
+public class BasicImageDownloader {
+}
---
+++ app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.birthdaycard
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity () {
+    override fun onCreate(saveInstanceState: Bundle?) {
+        super.onCreate(saveInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
